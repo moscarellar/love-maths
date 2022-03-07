d05be9b5b544ff28ec4f28bf89e93c35f682e2e0
moscarellar <moscarellar@gmail.com>
2022-03-07 12:33:20 +0000

    Set up structure functions for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+function runGame(){
+}
+
+function checkAnswer(){
+}
+
+function caclculateCorrectAnswer(){
+}
+
+function incrementScore(){
+}
+
+function incrementWrongAnswer(){
+}
+
+function displayAdditionQuestion(){
+}
+
+function displayAdditionquestion(){
+}
+
+function displaySubtractQuestion(){
+}
+
+function displayMultiplyQuestion(){
+}
